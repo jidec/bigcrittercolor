c8653fc0a893e8ff7c1695821b17f9c39e96bafd
Jacob Idec <jacob.idec@ufl.edu>
2024-03-20 21:14:12 -0400

    updated createBCCDataFolder to also update existing folders with new dirs

--- src/bigcrittercolor/createBCCDataFolder.py
+++ src/bigcrittercolor/createBCCDataFolder.py
@@ -11,37 +11,41 @@ def createBCCDataFolder(parent_folder, new_folder_name="bcc_data"):
 
     base_path = parent_folder + "/" + new_folder_name
 
+    def mkdir_if_new(path):
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
     # make folder
-    os.mkdir(base_path)
+    mkdir_if_new(base_path)
 
     # make images folders
-    os.mkdir(base_path + "/all_images")
-    os.mkdir(base_path + "/segments")
-    os.mkdir(base_path + "/masks")
-    os.mkdir(base_path + "/patterns")
+    mkdir_if_new(base_path + "/all_images")
+    mkdir_if_new(base_path + "/segments")
+    mkdir_if_new(base_path + "/masks")
+    mkdir_if_new(base_path + "/patterns")
 
     # make other folder
-    os.mkdir(base_path + "/other")
+    mkdir_if_new(base_path + "/other")
 
     # make inat_download_records folder
-    os.mkdir(base_path + "/other/inat_download_records")
+    mkdir_if_new(base_path + "/other/inat_download_records")
 
     # make ml checkpoints
-    os.mkdir(base_path + "/other/ml_checkpoints")
+    mkdir_if_new(base_path + "/other/ml_checkpoints")
 
     # make raw records folder
-    os.mkdir(base_path + "/other/raw_records")
+    mkdir_if_new(base_path + "/other/raw_records")
 
     # make processing info folder
-    os.mkdir(base_path + "/other/processing_info")
+    mkdir_if_new(base_path + "/other/processing_info")
     with open(base_path + "/other/processing_info/failed_mask_infers.txt", 'w') as file:
         pass  # No need to write anything, just create the file
 
     # make dirs for new classifier training
-    os.mkdir(base_path + "/other/filter_training/")
-    os.mkdir(base_path + "/other/filter_training/test")
-    os.mkdir(base_path + "/other/filter_training/test/good")
-    os.mkdir(base_path + "/other/filter_training/test/bad")
-    os.mkdir(base_path + "/other/filter_training/train")
-    os.mkdir(base_path + "/other/filter_training/train/good")
-    os.mkdir(base_path + "/other/filter_training/train/bad")
+    mkdir_if_new(base_path + "/other/filter_training/")
+    mkdir_if_new(base_path + "/other/filter_training/test")
+    mkdir_if_new(base_path + "/other/filter_training/test/good")
+    mkdir_if_new(base_path + "/other/filter_training/test/bad")
+    mkdir_if_new(base_path + "/other/filter_training/train")
+    mkdir_if_new(base_path + "/other/filter_training/train/good")
+    mkdir_if_new(base_path + "/other/filter_training/train/bad")
