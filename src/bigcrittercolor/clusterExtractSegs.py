93d840d404164b09558ce9dcb024290decc3ba8f
Jacob Idec <jacob.idec@ufl.edu>
2024-03-20 21:52:19 -0400

    adding non-taxonomic batching to clusterExtract

--- src/bigcrittercolor/clusterExtractSegs.py
+++ src/bigcrittercolor/clusterExtractSegs.py
@@ -18,7 +18,8 @@ from bigcrittercolor.helpers.image import _blobPassesFilter, _maskIsEmpty, _form
 # Pattern - segments reduced to color data
 
 # Extract segments using masks, normalize them, filter them using simple metrics, cluster using features from a pretrained CNN feature extractor
-def clusterExtractSegs(img_ids=None, sample_n=None,
+#@profile
+def clusterExtractSegs(img_ids=None, sample_n=None, batch_size=None,
     color_format_to_cluster = "grey", used_aux_segmodel=False,
     filter_hw_ratio_minmax = None, filter_prop_img_minmax = (0.01, 0.5), filter_symmetry_min = None, filter_intersects_sides=True, # filters
     mask_normalize_params_dict={'lines_strategy':"ellipse"}, # normalization/verticalization of masks
@@ -55,166 +56,178 @@ def clusterExtractSegs(img_ids=None, sample_n=None,
         if sample_n is not None:
             img_ids = random.sample(img_ids,sample_n)
 
-    _bprint(print_steps,"Filtering " + str(len(img_ids)) + " segments by specified criterion...")
-    masks = []
-    mask_ids = []
-    failed_hw_ids = []
-    failed_prop_ids = []
-    failed_sym_ids = []
-    failed_edge_ids = []
+    # batching is necessary when cluster filtering large numbers of images (>10000)
+    # not batching in this case will overload memory in the clustering step
+    batches = [img_ids]
+    if batch_size is not None:
+        # split list into list of lists (batches)
+        batches = []
+        # iterate over the image IDs, stepping by the batch size
+        for i in range(0, len(img_ids), batch_size):
+            batches.append(img_ids[i:i + batch_size])
 
-    # for each image id
-    for index, id in enumerate(img_ids):
-        if index % 100 == 0:
-            _bprint(print_steps, str(index) + "/" + str(len(img_ids)))
+    for img_ids in batches:
+        _bprint(print_steps,"Filtering " + str(len(img_ids)) + " segments by specified criterion...")
+        masks = []
+        mask_ids = []
+        failed_hw_ids = []
+        failed_prop_ids = []
+        failed_sym_ids = []
+        failed_edge_ids = []
 
-        # read image
-        mask = _readBCCImgs(id, type="mask",data_folder=data_folder)
-
-        # may have to convert this to binary
-
-        # if mask fails to load at all
-        if mask is None: continue
-
-        # if image has no white (mask is empty)
-        if _maskIsEmpty(mask):
-            continue
-
-        # keep in mind that currently these filters include verticalize I believe
-        # prop filter
-        if filter_prop_img_minmax is not None:
-            if not _blobPassesFilter(mask, prop_img_minmax=filter_prop_img_minmax, prevert=used_aux_segmodel, show=show_indv):
-                failed_prop_ids.append(id)
-                continue
-
-        # vert hw ratio filter
-        if filter_hw_ratio_minmax is not None:
-            if not _blobPassesFilter(mask, hw_ratio_minmax=filter_hw_ratio_minmax, prevert=used_aux_segmodel, show=show_indv):
-                failed_hw_ids.append(id)
-                continue
-
-        # sym filter
-        if filter_symmetry_min is not None:
-            if not _blobPassesFilter(mask, rot_invar_sym_min=filter_symmetry_min, prevert=used_aux_segmodel, show=show_indv):
-                failed_sym_ids.append(id)
-                continue
-
-        if filter_intersects_sides:
-            if not _blobPassesFilter(mask, intersects_sides=True):
-                failed_edge_ids.append(id)
-                continue
-
-        masks.append(mask)
-        mask_ids.append(id)
-
-    # print and show info about which masks passed
-    _bprint(print_steps, str(len(masks)) + " masks passed all filters")
-    show_type = "mask"
-    if filter_prop_img_minmax is not None:
-        _bprint(print_steps, str(len(failed_prop_ids)) + " masks failed for proportion of image")
-        if show: showBCCImages(img_ids=failed_prop_ids, show_type=show_type, sample_n=18, title="Failed Prop",data_folder=data_folder)
-    if filter_hw_ratio_minmax is not None:
-        _bprint(print_steps, str(len(failed_hw_ids)) + " masks failed for polygon-verticalized height-to-width ratio")
-        if show: showBCCImages(img_ids=failed_hw_ids, show_type=show_type, sample_n=18, title="Failed H/W",data_folder=data_folder)
-    if filter_symmetry_min is not None:
-        _bprint(print_steps, str(len(failed_sym_ids)) + " masks failed for symmetry")
-        if show: showBCCImages(img_ids=failed_sym_ids, show_type=show_type, sample_n=18, title="Failed Sym",data_folder=data_folder)
-    if filter_intersects_sides:
-        _bprint(print_steps, str(len(failed_edge_ids)) + " masks failed for edge intersection")
-        if show: showBCCImages(img_ids=failed_edge_ids, show_type=show_type, sample_n=18, title="Failed Edge",data_folder=data_folder)
-
-    masks_unchanged = masks.copy() # save unchanged masks to use for seg extraction later
-
-    # if aux segmodel was used, we would take the segments in masks folder and greyscale or binarize them
-    # otherwise we would get the raw segs then either greyscale or binarize them
-    # note that make_3channel is True because verticalizeImg takes 3 channel images
-    if used_aux_segmodel:
-        segs = _readBCCImgs(img_ids=mask_ids,type="mask",color_format=color_format_to_cluster,make_3channel=True, data_folder=data_folder)
-    else:
-        segs = _readBCCImgs(img_ids=mask_ids,type="raw_seg",color_format=color_format_to_cluster,make_3channel=True,data_folder=data_folder)
-
-    _bprint(print_steps,"Normalizing segments for feature and segment extraction if specified...")
-    if not used_aux_segmodel:
-        for index, seg in enumerate(segs):
+        # for each image id
+        for index, id in enumerate(img_ids):
             if index % 100 == 0:
                 _bprint(print_steps, str(index) + "/" + str(len(img_ids)))
-            segs[index] = _verticalizeImg(seg, **mask_normalize_params_dict)
 
-    _bprint(print_steps, "Clustering masks by CNN-extracted features...")
+            # read image
+            mask = _readBCCImgs(id, type="mask",data_folder=data_folder)
 
-    ids_for_full_display = mask_ids.copy()
-    # if we used an aux segmodel, full display breaks so we give it None
-    if used_aux_segmodel:
-        ids_for_full_display = None
+            # may have to convert this to binary
 
-    # show is always true here because we have to show for user input
-    labels = _clusterByImgFeatures(segs, feature_extractor=feature_extractor, cluster_algo=cluster_algo, cluster_n=cluster_n,
-                                   full_display_ids=ids_for_full_display, full_display_data_folder=data_folder,
-                                   print_steps=print_steps, cluster_params_dict=cluster_params_dict, show=True)
+            # if mask fails to load at all
+            if mask is None: continue
 
-    if preselected_clusters_input is None:
-        # take user input selecting the cluster number to extract segments for
-        chosen_cluster_labels = input("Enter one or more cluster labels separated by commas and segments will be extracted for all images of those labels: ")
-        chosen_cluster_labels = chosen_cluster_labels.split(',')
-    else:
-        # if we give it a string of preselected clusters use those of course
-        chosen_cluster_labels = preselected_clusters_input.split(',')
+            # if image has no white (mask is empty)
+            if _maskIsEmpty(mask):
+                continue
 
-    indices = []
-    # get all indices matching the chosen cluster labels
-    for index, label in enumerate(labels):
-        if any(str(chosen_label) == str(label) for chosen_label in chosen_cluster_labels):
-            indices.append(index)
+            # keep in mind that currently these filters include verticalize I believe
+            # prop filter
+            if filter_prop_img_minmax is not None:
+                if not _blobPassesFilter(mask, prop_img_minmax=filter_prop_img_minmax, prevert=used_aux_segmodel, show=show_indv):
+                    failed_prop_ids.append(id)
+                    continue
 
-    kept_ids = [mask_ids[i] for i in indices]
+            # vert hw ratio filter
+            if filter_hw_ratio_minmax is not None:
+                if not _blobPassesFilter(mask, hw_ratio_minmax=filter_hw_ratio_minmax, prevert=used_aux_segmodel, show=show_indv):
+                    failed_hw_ids.append(id)
+                    continue
 
-    # if we used an aux segmodel, we can just write the kept RGB segs in "masks" to segments
-    if used_aux_segmodel:
-        segs = _readBCCImgs(kept_ids, type="mask", data_folder=data_folder)
+            # sym filter
+            if filter_symmetry_min is not None:
+                if not _blobPassesFilter(mask, rot_invar_sym_min=filter_symmetry_min, prevert=used_aux_segmodel, show=show_indv):
+                    failed_sym_ids.append(id)
+                    continue
 
-    # if we did not use an aux segmodel, we get the masks for the ids, apply them to the original images, and verticalize them
-    else:
-        masks = _readBCCImgs(kept_ids, type="mask", data_folder=data_folder)
+            if filter_intersects_sides:
+                if not _blobPassesFilter(mask, intersects_sides=True):
+                    failed_edge_ids.append(id)
+                    continue
 
-        # temporary fix to probably a jpg corruption issue
-        #for index, mask in enumerate(masks):
-        #    mask[(mask > 10)] = 255
-        #    mask[(mask < 10)] = 0
-        #    masks[index] = mask
+            masks.append(mask)
+            mask_ids.append(id)
 
-        # get all images to match the masks
-        parent_imgs = _readBCCImgs(kept_ids, type="img", data_folder=data_folder)
+        # print and show info about which masks passed
+        _bprint(print_steps, str(len(masks)) + " masks passed all filters")
+        show_type = "mask"
+        if filter_prop_img_minmax is not None:
+            _bprint(print_steps, str(len(failed_prop_ids)) + " masks failed for proportion of image")
+            if show: showBCCImages(img_ids=failed_prop_ids, show_type=show_type, sample_n=18, title="Failed Prop",data_folder=data_folder)
+        if filter_hw_ratio_minmax is not None:
+            _bprint(print_steps, str(len(failed_hw_ids)) + " masks failed for polygon-verticalized height-to-width ratio")
+            if show: showBCCImages(img_ids=failed_hw_ids, show_type=show_type, sample_n=18, title="Failed H/W",data_folder=data_folder)
+        if filter_symmetry_min is not None:
+            _bprint(print_steps, str(len(failed_sym_ids)) + " masks failed for symmetry")
+            if show: showBCCImages(img_ids=failed_sym_ids, show_type=show_type, sample_n=18, title="Failed Sym",data_folder=data_folder)
+        if filter_intersects_sides:
+            _bprint(print_steps, str(len(failed_edge_ids)) + " masks failed for edge intersection")
+            if len(failed_edge_ids) > 0:
+                if show: showBCCImages(img_ids=failed_edge_ids, show_type=show_type, sample_n=18, title="Failed Edge",data_folder=data_folder)
 
-        # zip masks and their parents
-        masks_parents = [(x, y) for x, y in zip(masks, parent_imgs)]
+        masks_unchanged = masks.copy() # save unchanged masks to use for seg extraction later
 
-        # extract segments using masks
-        segs = [cv2.bitwise_and(parent_img, parent_img, mask=cv2.cvtColor(mask,cv2.COLOR_RGB2GRAY).astype(np.uint8)) for mask, parent_img in masks_parents]
+        # if aux segmodel was used, we would take the segments in masks folder and greyscale or binarize them
+        # otherwise we would get the raw segs then either greyscale or binarize them
+        # note that make_3channel is True because verticalizeImg takes 3 channel images
+        if used_aux_segmodel:
+            segs = _readBCCImgs(img_ids=mask_ids,type="mask",color_format=color_format_to_cluster,make_3channel=True, data_folder=data_folder)
+        else:
+            segs = _readBCCImgs(img_ids=mask_ids,type="raw_seg",color_format=color_format_to_cluster,make_3channel=True,data_folder=data_folder)
 
-        #seg = np.copy(segs[0])
-        #seg[np.where((seg == [0, 0, 0]).all(axis=2))] = [0, 0, 255]
-        #cv2.imshow("0",seg)
-        #cv2.waitKey(0)
+        _bprint(print_steps,"Normalizing segments for feature and segment extraction if specified...")
+        if not used_aux_segmodel:
+            for index, seg in enumerate(segs):
+                if index % 100 == 0:
+                    _bprint(print_steps, str(index) + "/" + str(len(img_ids)))
+                segs[index] = _verticalizeImg(seg, **mask_normalize_params_dict)
 
-        segs = [_verticalizeImg(seg, **mask_normalize_params_dict) for seg in segs]
+        _bprint(print_steps, "Clustering masks by CNN-extracted features...")
 
-        #seg = np.copy(segs[0])
-        #seg[np.where((seg == [0, 0, 0]).all(axis=2))] = [0, 0, 255]
-        #cv2.imshow("1", seg)
-        #cv2.waitKey(0)
+        ids_for_full_display = mask_ids.copy()
+        # if we used an aux segmodel, full display breaks so we give it None
+        if used_aux_segmodel:
+            ids_for_full_display = None
 
-        # make seg 4 channel
-        #segs = [_format(seg,)]
+        # show is always true here because we have to show for user input
+        labels = _clusterByImgFeatures(segs, feature_extractor=feature_extractor, cluster_algo=cluster_algo, cluster_n=cluster_n,
+                                       full_display_ids=ids_for_full_display, full_display_data_folder=data_folder,
+                                       print_steps=print_steps, cluster_params_dict=cluster_params_dict, show=True)
 
-    # zip segments and their ids
-    segs_ids = [(x, y) for x, y in zip(segs, kept_ids)]
-    # write each segment naming it by its ID
-    for seg_and_id in segs_ids:
-        dest = data_folder + "/segments/" + seg_and_id[1] + "_segment.png"
-        cv2.imwrite(dest, seg_and_id[0])
+        if preselected_clusters_input is None:
+            # take user input selecting the cluster number to extract segments for
+            chosen_cluster_labels = input("Enter one or more cluster labels separated by commas and segments will be extracted for all images of those labels: ")
+            chosen_cluster_labels = chosen_cluster_labels.split(',')
+        else:
+            # if we give it a string of preselected clusters use those of course
+            chosen_cluster_labels = preselected_clusters_input.split(',')
 
-    _bprint(print_steps, "Finished")
+        indices = []
+        # get all indices matching the chosen cluster labels
+        for index, label in enumerate(labels):
+            if any(str(chosen_label) == str(label) for chosen_label in chosen_cluster_labels):
+                indices.append(index)
 
+        kept_ids = [mask_ids[i] for i in indices]
+
+        # if we used an aux segmodel, we can just write the kept RGB segs in "masks" to segments
+        if used_aux_segmodel:
+            segs = _readBCCImgs(kept_ids, type="mask", data_folder=data_folder)
+
+        # if we did not use an aux segmodel, we get the masks for the ids, apply them to the original images, and verticalize them
+        else:
+            masks = _readBCCImgs(kept_ids, type="mask", data_folder=data_folder)
+
+            # temporary fix to probably a jpg corruption issue
+            #for index, mask in enumerate(masks):
+            #    mask[(mask > 10)] = 255
+            #    mask[(mask < 10)] = 0
+            #    masks[index] = mask
+
+            # get all images to match the masks
+            parent_imgs = _readBCCImgs(kept_ids, type="img", data_folder=data_folder)
+
+            # zip masks and their parents
+            masks_parents = [(x, y) for x, y in zip(masks, parent_imgs)]
+
+            # extract segments using masks
+            segs = [cv2.bitwise_and(parent_img, parent_img, mask=cv2.cvtColor(mask,cv2.COLOR_RGB2GRAY).astype(np.uint8)) for mask, parent_img in masks_parents]
+
+            #seg = np.copy(segs[0])
+            #seg[np.where((seg == [0, 0, 0]).all(axis=2))] = [0, 0, 255]
+            #cv2.imshow("0",seg)
+            #cv2.waitKey(0)
+
+            segs = [_verticalizeImg(seg, **mask_normalize_params_dict) for seg in segs]
+
+            #seg = np.copy(segs[0])
+            #seg[np.where((seg == [0, 0, 0]).all(axis=2))] = [0, 0, 255]
+            #cv2.imshow("1", seg)
+            #cv2.waitKey(0)
+
+            # make seg 4 channel
+            #segs = [_format(seg,)]
+
+        # zip segments and their ids
+        segs_ids = [(x, y) for x, y in zip(segs, kept_ids)]
+        # write each segment naming it by its ID
+        for seg_and_id in segs_ids:
+            dest = data_folder + "/segments/" + seg_and_id[1] + "_segment.png"
+            cv2.imwrite(dest, seg_and_id[0])
+
+        _bprint(print_steps, "Finished (batch)")
+    _bprint(print_steps, "Finished (all batches)")
 def _claheEqualize(img):
     # We first create a CLAHE model based on OpenCV
     # clipLimit defines threshold to limit the contrast in case of noise in our image
@@ -238,3 +251,9 @@ def _claheEqualize(img):
     #plt.show()
 
     return(colorimage_clahe)
+
+#if __name__ == '__main__':
+#    clusterExtractSegs(img_ids=_getIDsInFolder("E:/aeshna_data/masks", sample_n=5000), used_aux_segmodel=True,
+#                       filter_hw_ratio_minmax=(3, 100), cluster_params_dict={'pca_n': None},
+#                       filter_prop_img_minmax=(0.01, 0.9),
+#                       data_folder="E:/aeshna_data")
