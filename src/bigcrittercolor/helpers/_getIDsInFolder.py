f306283b86725afcef1877594ffdad4f717d1b87
Jacob Idec <jacob.idec@ufl.edu>
2024-03-20 21:05:32 -0400

    added sample_n param to getIDsInFolder

--- src/bigcrittercolor/helpers/_getIDsInFolder.py
+++ src/bigcrittercolor/helpers/_getIDsInFolder.py
@@ -1,8 +1,11 @@
 import os
+import random
 
 from bigcrittercolor.helpers import _imgNameToID
 
-def _getIDsInFolder(folder):
+def _getIDsInFolder(folder,sample_n=None):
     files = os.listdir(folder)
     ids = [_imgNameToID(f) for f in files]
+    if sample_n is not None:
+        ids = random.sample(ids, sample_n)
     return(ids)
