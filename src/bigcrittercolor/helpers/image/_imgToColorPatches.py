962154e7d6227a7f88fb2bb3eb049c822ca96dca
Jacob Idec <jacob.idec@ufl.edu>
2024-04-19 17:49:12 -0400

    fixed weird red clusters in cielab space

--- src/bigcrittercolor/helpers/image/_imgToColorPatches.py
+++ src/bigcrittercolor/helpers/image/_imgToColorPatches.py
@@ -10,16 +10,12 @@ from collections import Counter
 # this is used in clusterColorsFromPatterns
 def _imgToColorPatches(img, bg_mask=None, cluster_args={'n':10, 'algo':'kmeans'}, input_colorspace="rgb", use_median=False, min_patch_pixel_area = 5, return_patch_masks_colors_imgshapes=False, show=False):
 
-    #if input_colorspace is not "rgb":
-    #    img = _format._format(img, in_format=input_colorspace,out_format="rgb",alpha=False)
     shape = img.shape
     original_shape = img.shape[:2]  # height, width
 
-    # Reshape the image to a 2D array of pixels
-    pixel_values = img.reshape((-1, 3))
-    pixel_values = np.float32(pixel_values)
+    # reshape the image to a 2D array of pixels
+    pixel_values = np.float32(img.reshape((-1, 3)))
 
-    # Call the _cluster function instead of cv2.kmeans
     labels = _cluster(pixel_values, **cluster_args)
 
     # Convert labels to a 1D array if necessary
@@ -30,39 +26,40 @@ def _imgToColorPatches(img, bg_mask=None, cluster_args={'n':10, 'algo':'kmeans'}
     patch_bool_masks = []
     patch_colors = []
 
-    # Process each cluster and overlay patches
+    # for each unique label (i.e. each cluster)
     for label in np.unique(labels):
-        # Create a mask for the current cluster
+        # create a mask for the current cluster
         mask = (labels.reshape(original_shape) == label).astype(np.uint8) * 255
 
-        # Find connected components in the mask
+        # find connected components in the mask
         num_labels, labels_im, stats, centroids = cv2.connectedComponentsWithStats(mask, connectivity=4,
-                                                                             ltype=cv2.CV_32S)
+                                                                           ltype=cv2.CV_32S)
+        # this shows details of what all the patches look like for a cluster
         if show:
-            img_rgb = _format._format(img, in_format=input_colorspace, out_format="rgb")
-            # Map component labels to hue val
+            # map component labels to hue val
             label_hue = np.uint8(179 * labels_im / np.max(labels_im))
             blank_ch = 255 * np.ones_like(label_hue)
 
-            # Create a colored image to visualize the components
+            # create a colored image to visualize the components
             labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
 
-            # Convert from HSV to BGR for display
+            # convert from HSV to BGR for display
             labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
 
-            # Set background label to black
+            # set background label to black
             labeled_img[label_hue == 0] = 0
 
-            # Overlay component numbers
+            # overlay component numbers
             for i in range(1, num_labels):  # Start from 1 to skip the background
                 cv2.putText(labeled_img, str(i), (int(centroids[i][0]), int(centroids[i][1])), cv2.FONT_HERSHEY_SIMPLEX,
                             0.5, (255, 255, 255), 2)
 
-            #_showImages(show, [mask,labeled_img], titles=["Patch Cluster Mask","Connected Components"])
+            _showImages(show, [mask,labeled_img], titles=["Patch Cluster Mask","Connected Components"])
 
-        for j in range(1, num_labels):  # Start from 1 to skip the background
+        # for each patch (connected component) within a cluster
+        for j in range(1, num_labels):
             if stats[j, cv2.CC_STAT_AREA] >= min_patch_pixel_area:
-                # Create a mask for the current component
+                # create a mask for the current component
                 component_mask = (labels_im == j).astype(np.uint8) * 255
 
                 if bg_mask is not None:
@@ -75,7 +72,12 @@ def _imgToColorPatches(img, bg_mask=None, cluster_args={'n':10, 'algo':'kmeans'}
                     masked_pixels = img[component_mask != 0]
                     mean_color = np.median(masked_pixels, axis=0)
 
-                # Apply the mean color to the component in the overlay image
+                # hack fix for weird reds in cielab
+                if input_colorspace == "cielab":
+                    if np.sum(mean_color) < 10:
+                        continue
+
+                # apply the mean color to the component in the overlay image
                 overlay_image[component_mask == 255] = mean_color
 
                 # _showImages(show, [component_mask,overlay_image], titles=["New Component Mask","Updated Overlay Image"])
