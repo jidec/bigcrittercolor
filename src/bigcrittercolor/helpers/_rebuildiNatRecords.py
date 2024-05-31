26c38ca7b94bdab9e263eabc9f951f92ebdc2f11
Jacob Idec <jacob.idec@ufl.edu>
2024-05-31 13:55:37 -0400

    fix rebuild empty data bug

--- src/bigcrittercolor/helpers/_rebuildiNatRecords.py
+++ src/bigcrittercolor/helpers/_rebuildiNatRecords.py
@@ -6,17 +6,21 @@ def _rebuildiNatRecords(data_folder):
     files = os.listdir(data_folder + "/other/inat_download_records")
 
     # filter the files based on the specified string
-    csv_files = [file for file in files if file.endswith('.csv') and "iNat_images-" in file]
+    csv_filenames = [file for file in files if file.endswith('.csv') and "iNat_images-" in file]
 
     # initialize an empty DataFrame to store the merged data
     merged_data = pd.DataFrame()
 
     # iterate over the CSV files and merge them
-    for csv_file in csv_files:
-        file_path = data_folder + "/other/inat_download_records/" + csv_file
-        #file_path = os.path.join(data_folder + "/other/inat-download_records", csv_file)
-        data = pd.read_csv(file_path)
-        merged_data = pd.concat([merged_data, data])
+    for csv_filename in csv_filenames:
+        file_path = data_folder + "/other/inat_download_records/" + csv_filename
+        try:
+            data = pd.read_csv(file_path)
+            # Skip empty dataframes
+            if not data.empty:
+                merged_data = pd.concat([merged_data, data])
+        except pd.errors.EmptyDataError:
+            print(f"Warning: {file_path} is empty and will be skipped.")
 
     # keep useful columns
     merged_data = merged_data[['obs_id', 'taxon', 'latitude', 'longitude', 'annotations',
