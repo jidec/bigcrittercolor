c2ce8e40d0aed771c341dead61af2058b3810c83
Jacob Idec <jacob.idec@ufl.edu>
2024-08-09 19:22:00 -0400

    update _blackBgToTransparent to take an additive threshold

--- src/bigcrittercolor/helpers/image/_blackBgToTransparent.py
+++ src/bigcrittercolor/helpers/image/_blackBgToTransparent.py
@@ -1,12 +1,16 @@
 import cv2
+import numpy as np
 
-# converts a 3-channel BGR or RGB image to a 4-channel PNG by making completely black pixels transparent
-def _blackBgToTransparent(img):
+# Converts a 3-channel BGR or RGB image to a 4-channel PNG by making black pixels transparent based on a threshold
+def _blackBgToTransparent(img, threshold=0):
     # Make sure the image has an alpha channel
     if img.shape[2] < 4:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 
-    # Set the alpha channel to fully transparent for black pixels
-    img[(img[:, :, 0] == 0) & (img[:, :, 1] == 0) & (img[:, :, 2] == 0), 3] = 0
+    # Calculate the sum of all channels
+    channel_sum = img[:, :, 0] + img[:, :, 1] + img[:, :, 2]
+
+    # Set the alpha channel to fully transparent for pixels below the threshold
+    img[channel_sum <= threshold, 3] = 0
 
     return img
