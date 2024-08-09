faab42f115be70ed49d5d7b659fabca58f96eae8
Jacob Idec <jacob.idec@ufl.edu>
2024-08-09 18:31:47 -0400

    Update __init__.py

--- src/bigcrittercolor/project/__init__.py
+++ src/bigcrittercolor/project/__init__.py
@@ -8,3 +8,4 @@ from bigcrittercolor.project.zipImageFolder import zipImageFolder
 from bigcrittercolor.project.readSpeciesBalancedIDs import readSpeciesBalancedIDs
 from bigcrittercolor.project.saveSpeciesBalancedImageIDs import saveSpeciesBalancedImageIDs
 from bigcrittercolor.project.createManualCodingsDataset import createManualCodingsDataset
+from bigcrittercolor.project.saveSpeciesGenusAverageImages import saveSpeciesGenusAverageImages
